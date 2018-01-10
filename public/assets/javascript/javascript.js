fa5cd1b505dc8b463eb66bf921f3e54a3c7354f3
Ernesto Hernandez <h_ernesto@ymail.com>
2018-01-09 17:14:42 -0800

    no require in jquery environment allowed

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    var request = require("request");
-    var cheerio = require("cheerio");
-    
-    $("#mainScraper").on("click", function(){
+        $("#mainScraper").on("click", function(){
         console.log("working button");
         $("#status").remove();
         
