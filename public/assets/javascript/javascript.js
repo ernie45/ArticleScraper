d58c079830869d32d30ed1d0eacd47c95e4b3469
Ernesto Hernandez <h_ernesto@ymail.com>
2018-01-12 15:30:50 -0800

    comments added

--- public/assets/javascript/javascript.js
+++ public/assets/javascript/javascript.js
@@ -1,26 +1,43 @@
 $(document).ready(function(){
-    $.getJSON("/", function(data){
-        if (data.length > 0){
-            $("#status").css("visibility", "hidden");
-        }
-    });
+
+    /** This button will handle the scraping **/ 
     $("#mainScraper").on("click", function(){
+
+        //** Remove status words upon clicking */
         $("#status").remove();
-        $.getJSON("/scrape", function(data) {
-        }); 
+        //** Allow the scrape path to be accessed by router */
+        $.ajax("/scrape", {
+            type: "GET"
+        }).then(function(data){
+            //** Once router gets info, do this */
+            console.log("yep");
+            page.reload();
+        });
     });
+
+    /** This button will handle saving the articles */
     $(".saveButton").on("click", function(){
+
+        /**Let user know saving has been made */
         alert("Saved this article");
+        /** Grab the id associated with this article */
         var _id = $(this).attr("id");
+        /** Allow router to use the path along with associated id */
         $.ajax("/saveArticle/" + _id, {
             type: "PUT",
             data: {"saved": true}
         }).then(function(data){
+            /** Once saved, let the user know */
             console.log("data saved");
         });
     });
+
+    /**This button handles deleting articles */
     $(".deleteArticle").on("click", function(){
+
+        /** Grab id of article to delete */
         var _id = $(this).attr("name");
+        /** Allow router to use the path to delete the article */
         $.ajax("/deleteArticle/" + _id, {
             type: "PUT",
             data: null
@@ -28,7 +45,14 @@ $(document).ready(function(){
             console.log("data deleted");
         });
     });
+
+    /**This button still doesn't work well, but should handle saving a note */
     $(".createNote").on("click", function(){
-        console.log("working button");
+        /** Let the server */
+        $.ajax("/createNote", {
+            type: "GET"
+        }).then(function(data){
+            console.log("uhu");
+        });
     });
 });
--- router.js
+++ router.js
@@ -1,49 +1,77 @@
+/** Dependencies */
 var request = require("request");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
 var db = require("./models");
 
+/** To be able to connect to the database using th mongoose ORM */
 mongoose.Promise = Promise;
+/** the stuff in quotes remains the same, just rename the articlesNotesdb to database of your choosing */
 mongoose.connect("mongodb://localhost/articlesNotesdb", {
   useMongoClient: true
 });
 
+/** This function will be exported to be used by the server */
 module.exports = function(app){
+    /** When routing to home page */
     app.get("/", function(req, res){
+        /** Search the database, and choose  only 10 */
         db.articles.find({}).limit(10).then(function(data){
+            /** Send the client the index handlebars along with articles to display */
             res.render("index", {articles: data});
         });
     });
+
+    /** When receiving this path from the client, it should have an id to save the article */
     app.put("/saveArticle/:_id", function(req, res){
         console.log(req.params);
         console.log(req.body);
-        // db.articles.update({_id: req.params._id}, {$set: {saved: false}});
+        // This uses the mongoose ORM to update the associated id to saved = true
         db.articles.findOneAndUpdate({_id: req.params._id}, {$set: {saved : true}}).catch(err => console.log(err));
     });
+
+    /** When receiving this path, search the database again */
     app.get("/savedArticles", function(req, res){
         db.articles.find({
             saved: true
         }).then(function(data){
-            // res.json(data);
+            /** When done searching, send the client the savedArticles handlebars with the saved articles */
             res.render("savedArticles", {articles: data})    
         });
         //res.render("savedArticles");
     });
-    app.get("/scrape", function(req, res){
-        request("https://www.nytimes.com/", function(error, response, html){
-            var $ = cheerio.load(html);
-            $(".story-heading").each(function(i, element){
-                var heading = $(this).text(); 
-                var hyperlink = $(this).children("a").attr("href") || "no link";
 
+    /** When receiving scrape path */
+    app.get("/scrape", function(req, res){
+        //** Use request to be able to grab the url of the website to scrape */
+        request("https://www.nytimes.com/", function(error, response, html){
+            /** Allow cheerio to be used just like jquery using dollar sign notation */
+            var $ = cheerio.load(html);
+            /** Accessing story-heading from the url above will allow us to grab from all existing with that class */
+            $(".story-heading").each(function(i, element){
+                /** Get the text */
+                var heading = $(this).text();
+                /** Get the link associated with that article */ 
+                var hyperlink = $(this).children("a").attr("href") || "no link";
                 console.log(hyperlink);
+                /** Use the mongoose ORM to populate our database with the info above */
                 db.articles.create({"heading": heading, "hyperlink": hyperlink})
             });
-            res.redirect('/');
         });
     });
+
+    /** When receiving the delete articles path, along with the id,  */
     app.put("/deleteArticle/:_id", function(req, res){
+        /** Use mongoose ORM to handle updating where id = req.params._id  and make it not saved */
         db.articles.findOneAndUpdate({_id: req.params._id}, {$set: {saved: false}}).catch(error => console.log(error));
-        res.redirect("/savedArticles");
+    });
+
+    /** When sent this path, save a note, this part doestn'' work well yet */
+    app.get("/noteCreate", function(req, res){
+        res.render("saveNote", null);
+    });
+    app.get("/createNote", function(req, res){
+        console.log("redirect not working");
+        window.location.href;
     });
 };
